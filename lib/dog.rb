e4842cb30328d672f5d84181227d54ea67810da3
nimetbetul <nbkaratas@students.chccs.k12.nc.us>
2018-07-27 15:34:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
 # dog.rb
+class Dog
+  def initialize(name)
+    @name = name
+end 
+ 
+def bark
+  #behavior
+  puts "Woof!"
+end
+
+def sit 
+  puts "The Dog is sitting" 
+  end 
+end
+
+fido Dog.new 
+puts fido.name 
+fido.bark
+fido.sit
+
